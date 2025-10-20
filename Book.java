4168f17f7fefa0ab150b6cc3d02b664b0d7b276e
Dilyaraaa19 <dilyarka00@mail.ru>
2025-10-20 23:31:33 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,10 @@
+public class Book {
+    public static void main(String[] args){
+        int booksCount = 30;
+
+        while (booksCount > 0){
+            System.out.println("Осталось прочитать книг " + booksCount);
+            booksCount --;
+        }
+    }
+}
---
+++ praktikum.java
@@ -0,0 +1,7 @@
+public class praktikum {
+    public static void main(String[] args){
+        for (int i = 1; i <= 12; i= i + 1 ){
+            System.out.println("еме");
+        }
+    }
+}
